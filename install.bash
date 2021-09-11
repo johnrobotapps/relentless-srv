0ad123a3c75cfb123c8394c038393b4da9de5f6a
johnrobotapps <starrysasquatch@gmail.com>
2021-09-11 17:06:21 -0500

    server installer

--- install.bash
+++ install.bash
@@ -3,7 +3,7 @@
 # with an Ubuntu 20.04 server using
 # mongodb, python 3, and pymongo
 
-# Get all the standard packages updated
+# Upgrade all the prepackaged work
 sudo apt upgrade
 
 # Add the Official MongoDB verion
@@ -13,4 +13,20 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb
 sudo apt update
 sudo apt install -y mongodb-org
 
+# Export a MongoDB start command
+echo "alias start_mongod=\"sudo systemctl start mongod\""
+
 # Install Python 3.9
+PYTHON_VERSION="3.9.7"
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+tar -zxvf Python-$PYTHON_VERSION.tgz
+
+cd Python-$PYTHON_VERSION
+./configure --enable-optimizations
+make -j $(nproc)
+sudo make install
+
+
+# Install PyMongo python package
+
