e3ac3c0309cd4290be4ff748ff6350187bf4f4b8
johnrobotapps <starrysasquatch@gmail.com>
2021-09-11 22:57:37 -0500

    pip

--- install.bash
+++ install.bash
@@ -14,14 +14,13 @@ sudo apt update
 sudo apt install -y mongodb-org
 
 # Export a MongoDB start command
-echo "alias start_mongod=\"sudo systemctl start mongod\""
+echo "alias start_mongod=\"sudo systemctl start mongod\"" >> ~/.bashrc
 
 # Install Python 3.9
 PYTHON_VERSION="3.9.7"
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 
 cwd=$(pwd)
-
 cd ~
 wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
 tar -zxvf Python-$PYTHON_VERSION.tgz
@@ -30,7 +29,15 @@ cd Python-$PYTHON_VERSION
 ./configure --enable-optimizations --with-ensurepip=install
 make -j $(nproc)
 sudo make altinstall
+
 sudo ln -s /usr/local/bin/python3.9 /usr/bin/python
+sudo ln -s /usr/local/bin/pip3.9 /usr/bin/pip
+
+pip install --upgrade pip
 
 # Install PyMongo python package
+pip install pymongo
+
+# Go back to starting location
+cd $cwd
 
