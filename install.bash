d72865453e3be28bab2b0e73131de2c5d1175a2f
johnrobotapps <starrysasquatch@gmail.com>
2021-09-11 23:50:06 -0500

    mongodb-org added

--- install.bash
+++ install.bash
@@ -7,6 +7,7 @@
 sudo apt upgrade -y
 
 # Add the Official MongoDB verion
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
 # Install MongoDB
