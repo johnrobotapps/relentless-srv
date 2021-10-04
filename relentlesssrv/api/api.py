93708fb66d898b670f855d47c68b785506cbec12
John Ossyra <starrysasquatch@gmail.com>
2021-10-04 04:40:25 -0500

    format improved

--- relentlesssrv/api/api.py
+++ relentlesssrv/api/api.py
@@ -14,34 +14,8 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-path_home = Path("/")
-
-path_apiv1 = path_home / Path("api/v1")
-
-path_foodjournal = path_apiv1 / Path(
-    "foodjournal"
-)
-
-path_exerciselibrary = path_apiv1 / Path(
-    "exerciselibrary"
-)
-
-path_logentry = path_foodjournal / Path(
-    "logentry"
-)
-
-
-routes = {
-    "home": path_home,
-    "foodjournal": path_foodjournal,
-    "exerciselibrary": path_exerciselibrary,
-    "logentry": path_logentry,
-}
-
-
 
 class FoodJournals:
-
 
     def __init__(self):
         self._dict = dict()
--- relentlesssrv/api/routes.py
+++ relentlesssrv/api/routes.py
@@ -2,33 +2,18 @@
 
 # Root setup
 path_home = Path("/")
-
 path_apiv1 = path_home / Path("api/v1")
 
-
 # Endpoints with data
-path_foodjournal = path_apiv1 / Path(
-    "foodjournal"
-)
-path_exerciselibrary = path_apiv1 / Path(
-    "exerciselibrary"
-)
-path_logentry = path_foodjournal / Path(
-    "logentry"
-)
-
-
+path_foodjournal = path_apiv1 / Path("foodjournal")
+path_exerciselibrary = path_apiv1 / Path("exerciselibrary")
+path_logentry = path_foodjournal / Path("logentry")
 
 # For export
 routes = {
-
     "home": path_home,
-
     "foodjournal": path_foodjournal,
-
     "exerciselibrary": path_exerciselibrary,
-
     "logentry": path_logentry,
-
 }
 
